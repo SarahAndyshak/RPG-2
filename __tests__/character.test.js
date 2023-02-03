1a58d22bb8e908d01c7c3655dccb7611a149c92e
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-02-02 16:07:18 -0800

    Test 1 good pass, strings must come first.

--- __tests__/character.test.js
+++ __tests__/character.test.js
@@ -2,10 +2,17 @@ import Character from "./../src/js/character.js";
 
 describe('Character', () => {
 
-  test('our character should include a value for inspiration and skill', () => {
-    const character = new Character(2, 2);
-    expect(character.inspirationLevel).toEqual(2);
-    expect(character.skillLevel).toEqual(2);
+  test('our character should include a value for art genre, inspiration level, skill level', () => {
+    const mainCharacter = new Character("Mannerism", 1, 0,);
+    expect(mainCharacter.artGenre).toEqual("Mannerism");
+    expect(mainCharacter.inspirationLevel).toEqual(1);
+    expect(mainCharacter.skillLevel).toEqual(0);
   });
+
+  // test('our character should have an art genre', () => {
+  //   const character = new Character(2, 2);
+  //   expect(character.inspirationLevel).toEqual(2);
+  //   expect(character.skillLevel).toEqual(2);
+  // });
 
 });
--- src/js/character.js
+++ src/js/character.js
@@ -1,6 +1,7 @@
 export default class Character {
-  constructor(inspirationLevel, skillLevel) {
-
-
+  constructor(artGenre) {
+    this.artGenre = artGenre;
+    this.inspirationLevel = 1; // equiv for level up
+    this.skillLevel = 0; // equiv to xp
   }
 }
