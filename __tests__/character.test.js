ff2830f3d35b84be14880c947b1bac3ae45def48
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-02-02 16:36:51 -0800

    Test 2 good fail.

--- __tests__/character.test.js
+++ __tests__/character.test.js
@@ -3,16 +3,15 @@ import Character from "./../src/js/character.js";
 describe('Character', () => {
 
   test('our character should include a value for art genre, inspiration level, skill level', () => {
-    const mainCharacter = new Character("Mannerism", 1, 0,);
+    const mainCharacter = new Character("Mannerism", 1, 0);
     expect(mainCharacter.artGenre).toEqual("Mannerism");
     expect(mainCharacter.inspirationLevel).toEqual(1);
     expect(mainCharacter.skillLevel).toEqual(0);
   });
 
-  // test('our character should have an art genre', () => {
-  //   const character = new Character(2, 2);
-  //   expect(character.inspirationLevel).toEqual(2);
-  //   expect(character.skillLevel).toEqual(2);
-  // });
+  test('increaseInspo should increase the inspirationLevel by 25', () => {
+    const mainCharacter = new Character("Mannerism", 1, 0);
+    expect(mainCharacter.increaseInspo()).toEqual(26); 
+  });
 
 });
--- src/js/character.js
+++ src/js/character.js
@@ -4,4 +4,12 @@ export default class Character {
     this.inspirationLevel = 1; // equiv for level up
     this.skillLevel = 0; // equiv to xp
   }
+
+  increaseInspo() {
+    // if (this.inspirationLevel < 1) {
+    //   return this.inspirationLevel + 25;
+    // }
+    // return this.inspirationLevel;
+  }
+
 }
